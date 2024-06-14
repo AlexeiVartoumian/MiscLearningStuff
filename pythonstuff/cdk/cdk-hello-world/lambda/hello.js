565c73694fa655045d636ee508b66e4ac5e04058
AlexeiVartoumian <alex.vartoumian@gmail.com>
2024-06-14 18:05:30 +0100

    do iac stuff w aws cdk & constructs

---
+++ pythonstuff/cdk/cdk-hello-world/lambda/hello.js
@@ -0,0 +1,8 @@
+
+exports.handler = async(event) => {
+    return {
+        statusCode: 200,
+        headers: {"Content-Type": "text/plain"},
+        body: JSON.stringify({message: "Hello, World!"}),
+    };
+};
