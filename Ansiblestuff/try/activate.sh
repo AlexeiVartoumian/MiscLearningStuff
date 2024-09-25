404677a10e184eaf6ee1a1c0334db1e4e96e3f02
AlexeiVartoumian <alex.vartoumian@gmail.com>
2024-09-25 20:44:38 +0100

    hm

---
+++ Ansiblestuff/try/activate.sh
@@ -0,0 +1,38 @@
+mkdir ansible_aws_package
+cd ansible_aws_package
+
+
+
+python3 -m venv ansible_venv
+source ansible_venv/bin/activate
+
+
+
+pip install ansible boto3 botocore
+ansible-galaxy collection install amazon.aws
+
+
+
+pip freeze > requirements.txt
+
+Create a wrapper script to set up the environment:
+
+chmod +x run_ansible.sh
+
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export PYTHONPATH="$SCRIPT_DIR/ansible_venv/lib/python3.*/site-packages:$PYTHONPATH"
+export ANSIBLE_COLLECTIONS_PATH="$SCRIPT_DIR/ansible_venv/lib/python3.*/site-packages/ansible_collections"
+"$SCRIPT_DIR/ansible_venv/bin/ansible-playbook" "$@"
+
+
+zip -r ansible_aws_package.zip ansible_venv requirements.txt run_ansible.sh test_aws.yml
+
+aws s3 cp ansible_aws_package.zip s3://your-bucket-name/
+
+
+aws s3 cp s3://your-bucket-name/ansible_aws_package.zip .
+unzip ansible_aws_package.zip
+
+
+./run_ansible.sh test_aws.yml
