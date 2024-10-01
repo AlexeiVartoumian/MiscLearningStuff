7e3a7c9f544366adc834c52405080b18af3f04d9
AlexeiVartoumian <alex.vartoumian@gmail.com>
2024-10-01 20:56:53 +0100

    a

--- OneDrive/Documents/MiscLearningStuff/Fargate/thing.sh
+++ OneDrive/Documents/MiscLearningStuff/Fargate/thing.sh
@@ -10,56 +10,27 @@ log_message() {
 
 log_message "Starting post-api-helpers.sh script"
 
-# Debug information
-log_message "CUSTOMIZATION: $CUSTOMIZATION"
-log_message "DEFAULT_PATH: $DEFAULT_PATH"
-log_message "Current directory: $(pwd)"
-log_message "Contents of current directory:"
-ls -la
+import boto3
+import ansible_runner
 
-# Activate the virtual environment
-if [ -f "$DEFAULT_PATH/api-helpers-venv/bin/activate" ]; then
-    source $DEFAULT_PATH/api-helpers-venv/bin/activate
-    log_message "Virtual environment activated"
-else
-    log_message "Error: Virtual environment not found"
-    exit 1
-fi
+def run_playbook():
+    # Assume role in the vended account
+    sts = boto3.client('sts')
+    assumed_role = sts.assume_role(
+        RoleArn=f"arn:aws:iam::{VENDED_ACCOUNT_ID}:role/AWSAFTExecution",
+        RoleSessionName="AFTAnsibleSession"
+    )
+    
+    # Set up credentials for the assumed role
+    credentials = assumed_role['Credentials']
+    os.environ['AWS_ACCESS_KEY_ID'] = credentials['AccessKeyId']
+    os.environ['AWS_SECRET_ACCESS_KEY'] = credentials['SecretAccessKey']
+    os.environ['AWS_SESSION_TOKEN'] = credentials['SessionToken']
 
-# Print Python packages
-log_message "Installed Python packages:"
-pip list
+    # Run Ansible playbook
+    result = ansible_runner.run(playbook='path/to/your/playbook.yml')
+    print(result.status)
+    print(result.stdout.read())
 
-# Change to the directory containing the Ansible playbook
-if [ -d "$DEFAULT_PATH/$CUSTOMIZATION/ansible_playbooks" ]; then
-    cd $DEFAULT_PATH/$CUSTOMIZATION/ansible_playbooks
-    log_message "Changed to ansible_playbooks directory"
-else
-    log_message "Error: ansible_playbooks directory not found"
-    exit 1
-fi
-
-log_message "Contents of ansible_playbooks directory:"
-ls -la
-
-# Run the Ansible playbook
-if [ -f hello_world.yml ]; then
-    log_message "Running Ansible playbook"
-    ansible-playbook hello_world.yml
-else
-    log_message "Error: hello_world.yml not found in $(pwd)"
-    exit 1
-fi
-
-# Deactivate the virtual environment
-deactivate
-log_message "Virtual environment deactivated"
-
-# Return to the original directory
-cd $DEFAULT_PATH
-log_message "Returned to original directory"
-
-log_message "post-api-helpers.sh script completed"
-
-# Disable command echoing
-set +x
+if __name__ == "__main__":
+    run_playbook()
